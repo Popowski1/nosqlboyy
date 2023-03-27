8e6193b5c4814918a6e1265db7e7c6c90630283a
Shane <shanepopowski@yahoo.com>
2023-03-27 17:58:39 -0500

    finished usercon.js, porting over code to thoughtcon.js after

--- controllers/usercon.js
+++ controllers/usercon.js
@@ -1,4 +1,4 @@
-const {User} = require ('../models');
+const {User, Thought} = require ('../models');
 const userCon = {
 
     allUsers(req, res) {
@@ -44,6 +44,7 @@ addUser({
             res.status(400).json(err);
         });
 },
+
 updateUser({ params, body}, res) {
     User.findOneAndUpdate({
         _id: params._id}, body, {
@@ -62,4 +63,118 @@ updateUser({ params, body}, res) {
     
 },
 
+removeUser ({ params }, res) {
+    User.findOneAndDelete({
+        _id:params.id
+    })
+    .then (dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({
+                message: 'wrong user id.'
+            });
+            return;
+        }
+        return dbUserData;
+    })
+    .then(dbUserData => {
+        User.updateMany({
+                _id: {
+                 $in: dbUserData.friends
+                }
+            }, {
+                $pull: {
+                 friends: params.userId
+                }
+            })
+            .then(() => {
+            Thought.deleteMany({
+               username: dbUserData.username
+                    })
+              .then(() => {
+                  res.json({
+              message: 'User deleted successfully'
+                        });
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        res.status(400).json(err);
+                    })
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            })
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    })
+},
+
+addFriend({params}, res) {
+        User.findOneAndUpdate({
+            _id: params.userId}, {
+                $push: {
+                    friends: params.friendId
+                }
+            }, {
+                new: true
+            })
+            .then(dbUserData => {
+                if(!dbUserData) {
+                    res.status(404).json({
+                        message:'wrong user id.'
+                    });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err)
+                res.json(err)
+            
+        });
+    
+
+    },
+    removeFriend({params}, res) {
+        User.findOneAndDelete({
+            _id: params.thoughtId
+        })
+        .then(deletedFriend => {
+            if(!deletedFriend) {
+                return res.status(404).json({
+                    message:'wrong friend id.'
+                })
+            }
+            return User.findOneAndUpdate({
+                friends: params.friendIs
+            }, {
+                $pull: {
+                    friends: params.friendId
+                }
+            }, {
+                new: true
+            });
+        }) 
+            .then(dbUserData => {
+                if(!dbUserData) {
+                    res.status(404).json({
+                        message:'wrong user id.'
+                    });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err)
+                res.json(err)
+            
+        });
+    
+
+    },
+
 }
+
+module.exports = userController;
